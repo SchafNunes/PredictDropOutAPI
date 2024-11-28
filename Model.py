f68d0d126c313c6ee516b915c51d8866e8d68acf
Joao Vitor Schaf Nunes <joaovitorschafriva@gmail.com>
2024-11-28 10:26:25 -0300

    Adicionando novas Features

--- Model.py
+++ Model.py
@@ -10,18 +10,27 @@ from xgboost import XGBClassifier
 
 label_encoder = LabelEncoder()
 
-df = pd.read_csv('main.csv')
-df = df[df['Target'] != 'Enrolled']
+df = pd.read_csv('libs/main.csv')
 
-df['Target'] = label_encoder.fit_transform(df['Target'])
+df.columns = ["Estado civil", "Modo de inscricao", "Ordem de inscricao", "Curso", "Periodo (diurno/noturno)", "Qualificacao anterior", "Nacionalidade", "Escolaridade da mae", "Escolaridade do pai", "Ocupacao da mae", "Ocupacao do pai", "Deslocado", "Necessidades educacionais especiais", "Inadimplente", "Mensalidades em dia", "Genero", "Bolsista", "Idade na matricula", "Internacional", "Unidades curriculares 1 semestre (creditadas)", "Unidades curriculares 1 semestre (matriculadas)", "Unidades curriculares 1 semestre (avaliadas)", "Unidades curriculares 1 semestre (aprovadas)", "Nota das unidades curriculares 1 semestre", "Unidades curriculares 1 semestre (nao avaliadas)", "Unidades curriculares 2 semestre (creditadas)", "Unidades curriculares 2 semestre (matriculadas)", "Unidades curriculares 2 semestre (avaliadas)", "Unidades curriculares 2 semestre (aprovadas)", "Nota das unidades curriculares 2 semestre", "Unidades curriculares 2 semestre (nao avaliadas)", "Taxa de desemprego", "Taxa de inflacao", "PIB", "Resultado"]
+
+df.to_csv('libs/mainPTBR.csv', index=False)
+
+df = pd.read_csv('libs/mainPTBR.csv')
+
+# print(df)
+df = df[df['Resultado'] != 'Enrolled']
+
+df['Resultado'] = label_encoder.fit_transform(df['Resultado'])
 
 classes = label_encoder.classes_
 print(f'Classes: {classes}')
 for index, class_name in enumerate(classes):
     print(f'{class_name}: {index}')
 
-x = df.drop('Target', axis=1)
-y = df['Target']
+x = df.drop('Resultado', axis=1)
+y = df['Resultado']
+
 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, stratify = y,  random_state = 5)
 
@@ -49,9 +58,9 @@ f1 = f1_score(y_teste, preds, average='weighted')
 
 print(f1)
 
-joblib.dump(bayes_search, 'Dropout.pkl')
+joblib.dump(bayes_search, 'libs/Dropout.pkl')
 
-modelo = joblib.load('Dropout.pkl')
+modelo = joblib.load('libs/Dropout.pkl')
 
 dados_entrada = np.array([[1,8,5,2,1,1,1,13,10,6,10,1,0,0,1,1,0,20,0,0,0,0,0,0.0,0,0,0,0,0,0.0,0,10.8,1.4,1.74],[1,6,1,11,1,1,1,1,3,4,4,1,0,0,0,1,0,19,0,0,6,6,6,14.0,0,0,6,6,6,13.666666666666666,0,13.9,-0.3,0.79]])
 
--- api.py
+++ api.py
@@ -6,25 +6,25 @@ import joblib
 
 app = Flask(__name__)
 
-modelo = joblib.load('Dropout.pkl')
+modelo = joblib.load('libs/Dropout.pkl')
 
-@app.route("/predict_drop_out", methods=['POST'])
+
+@app.route("/predict_dropout", methods=['POST'])
 def predict_titanic():
 
-    features = ['Marital status', 'Application mode', 'Application order', 'Course', 'Daytime/evening attendance', 'Previous qualification', 'Nacionality', "Mother's qualification", "Father's qualification", "Mother's occupation", "Father's occupation", 'Displaced', 'Educational special needs', 'Debtor', 'Tuition fees up to date', 'Gender', 'Scholarship holder', 'Age at enrollment', 'International', 'Curricular units 1st sem (credited)', 'Curricular units 1st sem (enrolled)', 'Curricular units 1st sem (evaluations)', 'Curricular units 1st sem (approved)', 'Curricular units 1st sem (grade)', 'Curricular units 1st sem (without evaluations)', 'Curricular units 2nd sem (credited)', 'Curricular units 2nd sem (enrolled)', 'Curricular units 2nd sem (evaluations)', 'Curricular units 2nd sem (approved)', 'Curricular units 2nd sem (grade)', 'Curricular units 2nd sem (without evaluations)', 'Unemployment rate', 'Inflation rate', 'GDP']
+    features = ["Estado civil", "Modo de inscricao", "Ordem de inscricao", "Curso", "Periodo (diurno/noturno)", "Qualificacao anterior", "Nacionalidade", "Escolaridade da mae", "Escolaridade do pai", "Ocupacao da mae", "Ocupacao do pai", "Deslocado", "Necessidades educacionais especiais", "Inadimplente", "Mensalidades em dia", "Genero", "Bolsista", "Idade na matricula", "Internacional", "Unidades curriculares 1 semestre (creditadas)", "Unidades curriculares 1 semestre (matriculadas)", "Unidades curriculares 1 semestre (avaliadas)", "Unidades curriculares 1 semestre (aprovadas)", "Nota das unidades curriculares 1 semestre", "Unidades curriculares 1 semestre (nao avaliadas)", "Unidades curriculares 2 semestre (creditadas)", "Unidades curriculares 2 semestre (matriculadas)", "Unidades curriculares 2 semestre (avaliadas)", "Unidades curriculares 2 semestre (aprovadas)", "Nota das unidades curriculares 2 semestre", "Unidades curriculares 2 semestre (nao avaliadas)", "Taxa de desemprego", "Taxa de inflacao", "PIB"]
 
     dados = request.json
 
     if not all(feature in dados for feature in features):
-        return jsonify("Dicionário com informação incompleta")
+        return jsonify("Dicionario com informacao incompleta")
         
     try:
-
         df = pd.DataFrame(dados, index=[0])
         resultado = modelo.predict(df)
 
         response = {
-            'descrição': 'Evasão' if resultado[0] == 1 else 'Matriculado'
+            'descricao': 'Evasao' if resultado[0] == 0 else 'Graduado'
         }
     
     except Exception as e:

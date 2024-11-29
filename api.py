dabc01c4d44dfbfdaa12c96c12bdc48194fe9da7
Jnzinho <joaovitorjn@unochapeco.edu.br>
2024-11-28 22:17:30 -0300

    fix: cors

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 
 import pandas as pd
 
 import joblib
 
 app = Flask(__name__)
+CORS(app)  # Initialize CORS
 
 modelo = joblib.load('libs/Dropout.pkl')
 

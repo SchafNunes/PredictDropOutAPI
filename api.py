396c65a474e5342fe5df0f5b74e82d2907780954
Jnzinho <joaovitorjn@unochapeco.edu.br>
2024-12-02 19:17:47 -0300

    fix:  corrigir cors

--- api.py
+++ api.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 
 import pandas as pd
 
 import joblib
 
 app = Flask(__name__)
-CORS(app)  # Initialize CORS
+CORS(app, resources={r"/*": {"origins": "*"}})  # Explicitly allow all origins
 
 modelo = joblib.load('libs/Dropout.pkl')
 
